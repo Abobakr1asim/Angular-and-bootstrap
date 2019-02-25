48ef823b47f583e14d3347c99427f3a6d3c583d1
abobakr <asim1bakr@gmail.com>
2019-02-25 13:19:05 +0200

    add app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AddComponent } from './add/add.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AllcatsComponent } from './allcats/allcats.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { CardsComponent } from './cards/cards.component';
+
+const routes : Routes=[
+  {path:'',redirectTo:'home',pathMatch:'full' },
+  {path:'home',component:HomeComponent},
+  {path:'add',component:  AddComponent },
+  {path:'login',component: LoginComponent},
+  {path:'register',component: RegisterComponent},
+  {path:'all cats',component: AllcatsComponent},
+  {path:'card',component: CardsComponent}
+  
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AddComponent,
+    LoginComponent,
+    RegisterComponent,
+    AllcatsComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCtq4l4EgUAaEltJJ-tW1BDuebJON7BzYY",
+    authDomain: "my-cat-bb5fd.firebaseapp.com",
+    databaseURL: "https://my-cat-bb5fd.firebaseio.com",
+    projectId: "my-cat-bb5fd",
+    storageBucket: "my-cat-bb5fd.appspot.com",
+    messagingSenderId: "203169065628"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
